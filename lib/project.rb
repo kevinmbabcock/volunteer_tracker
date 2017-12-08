b981d8756fb52f73c92decc50ad976947eff0596
Kevin Babcock <kevinmbabcock@icloud.com>
2017-12-08 11:30:42 -0800

    add .all #save #== methods to Project class

--- lib/project.rb
+++ lib/project.rb
@@ -6,5 +6,25 @@ class Project
     @id = attributes.fetch(:id)
   end
 
+  def self.all
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each do |project|
+      name = project.fetch("name")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:name => name, :id => id}))
+    end
+    projects
+  end
+
+  def save
+    result = DB.exec("INSERT INTO projects (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first.fetch("id").to_i
+  end
+
+  def ==(other_project)
+    self.name.==(other_project.name).&(self.id.==(other_project.id))
+  end
+
 
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -9,5 +9,29 @@ describe(Project) do
     end
   end
 
+  describe(".all") do
+    it("is empty in the beginning") do
+      expect(Project.all).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("correctly saves a new project to the database") do
+      new_project = Project.new({:name => "project1", :id => nil})
+      new_project.save
+      expect(Project.all).to(eq([new_project]))
+    end
+  end
+
+  describe("#==") do
+    it("evaluates projects as equal if they have the same name") do
+      new_project1 = Project.new({:name => "my_project", :id => nil})
+      new_project2 = Project.new({:name => "my_project", :id => nil})
+      expect(new_project1).to(eq(new_project2))
+    end
+  end
+
+
+
 
 end
