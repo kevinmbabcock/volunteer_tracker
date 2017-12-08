df720ea31e51f03d99cee31cedc52f17f34f12f1
Kevin Babcock <kevinmbabcock@icloud.com>
2017-12-08 13:52:26 -0800

    add forms for user to add projects and volunteers to database

--- app.rb
+++ app.rb
@@ -11,3 +11,40 @@ DB = PG.connect({:dbname => "volunteer_tracker"})
 get('/') do
   erb(:index)
 end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/projects/add-project') do
+
+  erb(:add_project)
+end
+
+post('/projects') do
+  name = params.fetch("projectName")
+  new_project = Project.new({:name => name, :id => nil})
+  new_project.save
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/volunteers') do
+  @volunteers = Volunteer.all
+  erb(:volunteers)
+end
+
+get('/volunteers/add-volunteer') do
+
+  erb(:add_volunteer)
+end
+
+# post('/volunteers') do
+#   name = params.fetch("volunteerName")
+#   project_id = params.fetch("project_id").to_i
+#   @volunteer = Volunteer.new({:name => name, :project_id => project_id})
+#   @volunteer.save
+#   @project = Project.find(project_id)
+#   erb(:volunteers)
+# end
