369de9fb574343c9619e275196baaca3c7486bed
Kevin Babcock <kevinmbabcock@icloud.com>
2017-12-08 11:37:55 -0800

    add #initialize method for Volunteer class

--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -1,1 +1,14 @@
 require('spec_helper')
+
+describe(Volunteer) do
+
+  describe("#initialize") do
+    it("creates a volunteer") do
+      new_volunteer = Volunteer.new({:name => "Kevin", :id => nil})
+      expect(new_volunteer).to(be_an_instance_of(Volunteer))
+    end
+  end
+
+
+
+end
