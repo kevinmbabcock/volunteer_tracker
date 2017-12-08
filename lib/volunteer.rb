c19087308ffae22c055ee73be38c92b0d01f4dfb
Kevin Babcock <kevinmbabcock@icloud.com>
2017-12-08 12:19:34 -0800

    add .all #save #== methods to Volunteer class

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,10 +1,28 @@
 class Volunteer
-  attr_reader(:name, :id)
+  attr_reader(:name, :project_id)
 
   def initialize(attributes)
     @name = attributes.fetch(:name)
-    @id = attributes.fetch(:id)
+    @project_id = attributes.fetch(:project_id)
   end
 
+  def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each do |volunteer|
+      name = volunteer.fetch("name")
+      id = volunteer.fetch("project_id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => id}))
+    end
+    volunteers
+  end
+
+  def save
+    DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', '#{@project_id}');")
+  end
+
+  def ==(other_volunteer)
+    self.name.==(other_volunteer.name).&(self.project_id.==(other_volunteer.project_id))
+  end
 
 end
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -4,8 +4,32 @@ describe(Volunteer) do
 
   describe("#initialize") do
     it("creates a volunteer") do
-      new_volunteer = Volunteer.new({:name => "Kevin", :id => nil})
+      new_volunteer = Volunteer.new({:name => "Kevin", :project_id => 1})
       expect(new_volunteer).to(be_an_instance_of(Volunteer))
+    end
+  end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(Volunteer.all).to(eq([]))
+    end
+  end
+
+  describe("#save") do
+    it("correctly saves a volunteer to the database") do
+      new_volunteer = Volunteer.new({:name => "Kevin", :project_id => 1})
+      new_volunteer.save
+      expect(Volunteer.all).to(eq([new_volunteer]))
+    end
+  end
+
+  describe("#==") do
+    it("evaluates volunteers as equal if they ahve the same name and project id") do
+      new_volunteer1 = Volunteer.new({:name => "Kevin", :project_id => 1})
+      new_volunteer1.save
+      new_volunteer2 = Volunteer.new({:name => "Kevin", :project_id => 1})
+      new_volunteer2.save
+      expect(new_volunteer1).to(eq(new_volunteer2))
     end
   end
 
